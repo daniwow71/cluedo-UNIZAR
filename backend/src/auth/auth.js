113b1942548387bec093ccb3a83a9839d823892b
Samuel <samuelfelipelorente@gmail.com>
2025-02-21 20:50:31 +0100

    arreglados bugs en backend en login

--- backend/src/auth/auth.js
+++ backend/src/auth/auth.js
@@ -1,13 +1,13 @@
 import jwt from 'jsonwebtoken';
 
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
 dotenv.config();
 
-const TOKEN_SECRET = process.env.TOKEN_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET;
 const TOKEN_EXPIRATION = 86400;  // 24 hours
 
 const createToken = (user) => {
-  return jwt.sign({ id: user.id }, TOKEN_SECRET, {
+  return jwt.sign({ id: user.id }, JWT_SECRET, {
     expiresIn: TOKEN_EXPIRATION
   });
 }
