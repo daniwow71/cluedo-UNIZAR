a956cec3d852e46bb61f2bd2abeade6ec5d1ffea
sanagustin <daniader23@gmail.com>
2025-03-09 10:21:58 +0100

    Actualizacion y implementacion de la sincronizacion de los sequalice con la base de datos

--- backend/src/database/init.js
+++ backend/src/database/init.js
@@ -1,6 +1,19 @@
 import sequelize from './database.js';
 import { Sequelize } from 'sequelize';
-import UserModel from '../models/Usuario.js';
+import EstadoPartida from '../models/EstadoPartida.js';
+import Cuenta from '../models/Cuenta.js';
+import Usuario from '../models/Usuario.js';
+import Administrador from '../models/Administrador.js';
+import Tablero from '../models/Tablero.js';
+import Habitacion from '../models/Habitacion.js';
+import Puerta from '../models/Puerta.js';
+import Carta from '../models/Carta.js';
+import Partida from '../models/Partida.js';
+import SolucionPartida from '../models/SolucionPartida.js';
+import Jugador from '../models/Jugador.js';
+import PosicionJugador from '../models/PosicionJugador.js';
+import JugadorCarta from '../models/JugadorCarta.js';
+import Acusacion from '../models/Acusacion.js';
 
 const initDatabase = async () => {
   try {
@@ -13,8 +26,24 @@ const initDatabase = async () => {
 
     await tempSequelize.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`);
     await tempSequelize.close();
-    
-    await UserModel.sync();
+
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+
+    await EstadoPartida.sync();
+    await Cuenta.sync();
+    await Usuario.sync();
+    await Administrador.sync();
+    await Tablero.sync();
+    await Habitacion.sync();
+    await Puerta.sync();
+    await Carta.sync();
+    await Partida.sync();
+    await SolucionPartida.sync();
+    await Jugador.sync();
+    await PosicionJugador.sync();
+    await JugadorCarta.sync();
+    await Acusacion.sync();
 
     await sequelize.sync({ force: true });
     console.log('Database initialized successfully');
